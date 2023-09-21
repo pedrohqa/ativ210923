337871f41bf9bea2116c75c31708c0f1eedd3d30
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-09-21 11:13:21 -0300

    p

--- variaveis.php
+++ variaveis.php
@@ -3,23 +3,23 @@
     echo "<h3>Primeiro Exercicio</h3><br>";
 
     //variavel de tipo String
-    $nome = "meu nome é Pedro";
-    echo "$nome";
+    $nome1 = "meu nome é Pedro";
+    echo "$nome1";
     echo "<br><br>";
 
     //variavel de tipo Inteiro
-    $ano = 2023;
-    echo "$ano";
+    $ano1 = 2023;
+    echo "$ano1";
     echo "<br><br>";
 
     //variavel de tipo Float
-    $pi = 3.14159265;
-    echo "$pi";
+    $pi1 = 3.14159265;
+    echo "$pi1";
     echo "<br><br>";
 
     //variavel de tipo Booleano
-    $sim = "true";
-    echo "$sim";
+    $sim1 = "true";
+    echo "$sim1";
     echo "<br><br>";
 
     //Segundo Exercicio
@@ -27,62 +27,62 @@
     echo "<br>";
 
     //variaveis
-    $num = 23.8;
+    $num2 = 23.8;
 
     //is_int() Verifica se o número é inteiro
-    if(is_int($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_int($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_integer() Verifica se o número é inteiro
-    if(is_integer($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_integer($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_long() Verifica se a variavel e número inteiro ou não
-    if(is_long($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_long($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_float Verifica os numero que não é inteiro,ou seja, é quebrado
-    if(is_float($num)){
-        echo"$num nâo é Número Inteiro <br>";
+    if(is_float($num2)){
+        echo"$num2 nâo é Número Inteiro <br>";
     }
     else{
-        echo "$num é Número Inteiro <br>";
+        echo "$num2 é Número Inteiro <br>";
     }
 
     //is_string()
-    if(is_string($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_string($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_array()
-    if(is_array($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_array($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_object() Verifica se o número de ponto flutuante é real ou não
-    if(is_object($num)){
-        echo"$num é Número Inteiro <br>";
+    if(is_object($num2)){
+        echo"$num2 é Número Inteiro <br>";
     }
     else{
-        echo "$num nâo é Número Inteiro <br>";
+        echo "$num2 nâo é Número Inteiro <br>";
     }
 
     //is_real() Verifica se o número de ponto flutuante é real ou não, porem não funciona mais em php
@@ -98,7 +98,7 @@
     echo "<h3>Terceiro Exercicio</h3><br>";
 
     //variavel
-    $num2 = 2.5;    
-    settype($num,"integer");
-    echo "Valor Inteiro: $num2";
+    $num3 = 2.5;    
+    settype($num3,"integer");
+    echo "Valor Inteiro: $num3";
 ?>
